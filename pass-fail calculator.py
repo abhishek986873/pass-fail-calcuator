5efbc21a660fa3ac659b6403d7ea5681d30e9a23
abhishek986873 <110660543+abhishek986873@users.noreply.github.com>
2022-08-20 14:41:06 +0530

    Update pass-fail calculator.py

--- pass-fail calculator.py
+++ pass-fail calculator.py
@@ -3,14 +3,14 @@ print('---------------------------------------------------------------')
 print("* CRITERIA FOR PASSING:-\n* TOTAL 40% REQUIRED\n* EACH SUBJECT MARKS MUST GREATER THAN 33")
 print('---------------------------------------------------------------')
 # taking subject mark input 
-stud1=int(input('Enter maths mark: '))
-stud2=int(input('Enter physics marks: '))
-stud3=int(input('Enter chemistry marks: '))
+stud1=int(input('• Enter maths mark: '))
+stud2=int(input('• Enter physics marks: '))
+stud3=int(input('• Enter chemistry marks: '))
 print('---------------------------------------------------------------')
 # calculating percentage
 add=stud1+stud2+stud3
 div=(add/300)
-per_nt=(div*100)
+per_nt=int((div*100))
 # finding total mark percentage
 total_marks=stud1+stud2+stud3 #must be equal or greater than 120
 #each subject percent
